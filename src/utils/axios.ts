5af7c9d0278620b4377be3e3de455f66b66f0936
Oleksii But <alms27912@gmail.com>
2024-09-10 17:09:18 +0300

    fix api url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-// export default axios.create({
-//     baseURL: 'https://loki-api2.axels.com.ua/api',
-// });
-
 export default axios.create({
-    baseURL: 'http://localhost:4200/api',
+    baseURL: 'https://loki-api2.axels.com.ua/api',
 });
